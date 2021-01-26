db611277f53ced986b7017286261e739ea7e5baf
bhaktimp02 <bhaktimp02@gmail.com>
2021-01-26 13:02:44 -0500

    commit_01_26_02: - added html file and flask route

--- app_run.py
+++ app_run.py
@@ -1,1 +1,20 @@
 import flask
+from flask import Flask, render_template
+import sys
+import os
+
+app = Flask(__name__)
+print(os.pathdir)
+
+@app.route("/")
+def home():
+    return render_template("/Users/bhaktipatel/PycharmProjects/FlasTestapp/template/home.html")
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+
+if __name__ == "__main__":
+
+    app.run(port=5000, host='127.0.0.1', debug=True)
