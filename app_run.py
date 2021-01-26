3d6f0b4980d9a145d1a8992b46f1978e253ab7cb
bhaktimp02 <bhaktimp02@gmail.com>
2021-01-26 14:56:45 -0500

    commit_01_26_02: - added html file and flask route - added template_folder in app function

--- app_run.py
+++ app_run.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 import sys
 import os
 
-app = Flask(__name__)
-print(os.pathdir)
+app = Flask(__name__, template_folder='template')
+print(os.path)
 
 @app.route("/")
 def home():
-    return render_template("/Users/bhaktipatel/PycharmProjects/FlasTestapp/template/home.html")
+    return render_template('home.html')
 
 @app.route("/salvador")
 def salvador():
@@ -17,4 +17,4 @@ def salvador():
 
 if __name__ == "__main__":
 
-    app.run(port=5000, host='127.0.0.1', debug=True)
+    app.run(port=5000, host= '127.0.0.1', debug=True)
